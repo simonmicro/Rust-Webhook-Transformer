eedcc3d1ed5c7dd24677418c44901689d73c41c5
simonmicro <simon@simonmicro.de>
2023-07-02 13:29:11 +0200

    Implemented message forwarding and added default

--- src/transformer.rs
+++ src/transformer.rs
@@ -20,14 +20,29 @@ impl TransformerConfigTypes {
     }
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct HookshotMessage {
+    text: String, // automatically converted from Markdown to HTML
+    html: Option<String>, // if not provided, the text will be (converted and) used
+    username: Option<String>, // will be prepended to the message
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GrafanaToHookshotTransformer {
     uri: String,
-    just_show_message: bool
+    just_show_message: Option<bool>
 }
 impl GrafanaToHookshotTransformer {
-    async fn submit(&self, msg: &str) -> Result<(), String> {
-        debug!("TODO: Submit to {} -> {}", self.uri, msg);
+    async fn submit(&self, msg: &HookshotMessage) -> Result<(), String> {
+        debug!("Submitting message to Hookshot (via {}): {:#?}", self.uri, msg);
+        let client = reqwest::Client::new();
+        client.post(&self.uri)
+            .body(serde_json::to_string(msg).map_err(|e| e.to_string())?)
+            .header("Content-Type", "application/json")
+            .timeout(std::time::Duration::from_secs(10))
+            .send()
+            .await
+            .map_err(|e| e.to_string())?;
         Ok(())
     }
 
@@ -40,10 +55,15 @@ impl GrafanaToHookshotTransformer {
         let body = serde_json::from_str::<serde_json::Value>(body.as_str()).map_err(|e| "Failed to parse the body as JSON: ".to_string() + &e.to_string())?;
         let body = body.as_object().ok_or("The body is not a JSON object".to_string())?;
 
-        if self.just_show_message {
+        if self.just_show_message.unwrap_or(false) {
             let message = body.get("message").ok_or("The body does not contain a message".to_string())?;
             let message = message.as_str().ok_or("The message is not a string".to_string())?;
-            self.submit(message).await
+            let message = HookshotMessage {
+                text: message.to_string(), // Grafana already sends Markdown
+                html: None,
+                username: None
+            };
+            self.submit(&message).await
         } else {
             // TODO
             Err("TODO: Implement".to_string())
